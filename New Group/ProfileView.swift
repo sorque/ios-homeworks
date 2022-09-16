c9bbb234a480be60386f5fec1938d376ac2f5198
sorque <50024994+sorque@users.noreply.github.com>
2022-09-16 22:28:55 +0300

    Add files via upload

---
+++ New Group/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by m.korovin on 11.09.2022.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    
+    @IBOutlet weak var pugavatar: UIImageView!
+    @IBOutlet weak var pugname: UILabel!
+    @IBOutlet weak var pugfamilyname: UILabel!
+    @IBOutlet weak var pugcity: UILabel!
+    @IBOutlet weak var somerandeomtext: UITextView!
+}
---
+++ New Group/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments
+//
+//  Created by m.korovin on 16.09.2022.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
